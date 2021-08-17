23e81643d3cbf4c28542248e89c05c1db26d97d2
Vitold S <vit1251@gmail.com>
2021-08-18 02:32:02 +0300

    Working version

--- src/lib.rs
+++ src/lib.rs
@@ -1,32 +1,66 @@
 
+use log::{trace,debug};
 use std::fs::File;
+use std::io::Write;
 use std::io::LineWriter;
 
-struct AutoCfg {
-    config_dir: String
+struct Definition {
+    name: String,
+    defined: bool,
+    value: String,
+    optional: bool,
+}
+
+pub struct AutoCfg {
+    config_dir: String,
+    defines: Vec<Definition>,
 }
 
 impl AutoCfg {
 
-    fn new() -> AutoCfg {
-        AutoCfg {
-            config_dir: String::from("."),
-        }
+    pub fn new() -> AutoCfg {
+        let mut result = AutoCfg {
+            config_dir: String::from("config.h"),
+            defines: Vec::<Definition>::new(),
+        };
+        result.init();
+        result
     }
 
-    fn write(&self) -> Result<(), std::io::Error> {
+    fn init(&mut self) {
+        self.set_quoted("PACKAGE_NAME", "example");
+        self.set_quoted("VERSION", "1.0.0")
+    }
 
-        let mut stream = File::create(self.config_dir);
+    pub fn set(&mut self, name: &str, value: &str) {
+        trace!("set: name = {} value = {}", name, value);
+        // TODO - save in cache ...
+        self.defines.push(Definition{
+            name: String::from(name),
+            defined: true,
+            value: String::from(value),
+            optional: false,
+        });
+    }
+
+    pub fn set_quoted(&mut self, name: &str, value: &str) {
+        let new_value = format!("\"{}\"", value);
+        self.set(name, &new_value);
+    }
+
+    pub fn write(&self) -> Result<(), std::io::Error> {
+
+        let mut stream = File::create(&self.config_dir)?;
 
         let mut output = LineWriter::new(stream);
 
-        output.write_all("#define FOO_BAR_H 1\n");
+        for def in &self.defines {
+            let rec = format!("#define {} {}\n", def.name, def.value);
+            output.write_all(rec.as_bytes())?;
+        }
 
-        output.write_all(format!("#define PACKAGE_NAME \"{}\"\n", "example"));
 
-        output.write_all(format!("#define VERSION \"{}\"\n", "1.0.0"));
-
-        let _status = output.flush();
+        output.flush()?;
 
         Ok(())
     }
